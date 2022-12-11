8f9d2352416a651442b2958bdc26b7b40da251c3
sissi-shen <yaxuan_shen@ucsb.edu>
2022-12-10 17:53:35 -0800

    commit

--- model_building.R
+++ model_building.R
@@ -6,6 +6,7 @@ decision_tree_spec <- decision_spec %>%
 decision_tree_wf <- workflow() %>%
   add_model(decision_tree_spec %>% set_args(cost_complexity = tune())) %>%
   add_recipe(adapt_recipe)
+save(decision_tree_wf, file = "decision_tree_wf.rda")
 
 set.seed(1029)
 adapt_dt_grid <- grid_regular(cost_complexity(range = c(-3, -1)), levels = 10)
@@ -24,6 +25,7 @@ rf_spec <- rand_forest(mtry = tune(), trees = tune(), min_n = tune()) %>%
 rf_wf <- workflow() %>%
   add_model(rf_spec) %>%
   add_recipe(adapt_recipe)
+save(rf_wf, file = "rf_wf.rda")
 
 set.seed(1029)
 adapt_rf_grid <- grid_regular(mtry(range = c(1, 13)), trees(range = c(1, 10)), 
@@ -43,6 +45,7 @@ boost_spec <- boost_tree(mtry = tune(), trees = tune(), min_n = tune(), tree_dep
 boost_wf <- workflow() %>%
   add_model(boost_spec) %>%
   add_recipe(adapt_recipe)
+save(boost_wf, file = "boost_wf.rda")
 
 set.seed(1029)
 adapt_boost_grid <- grid_regular(mtry(range = c(1, 13)), trees(range = c(10, 2000)), 
@@ -62,6 +65,7 @@ knn_spec <- nearest_neighbor(neighbors = tune()) %>%
 knn_wf <- workflow() %>%
   add_model(knn_spec) %>%
   add_recipe(adapt_recipe)
+save(knn_wf, file = "knn_wf.rda")
 
 set.seed(1029)
 adapt_knn_grid <- grid_regular(neighbors(range = c(3, 15)), levels = 10)
